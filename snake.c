21333fde241e428c882dc34d9547f698587cbc61
Roman Slobodchikov <slb-rs@yandex.ru>
2021-02-03 01:55:23 +0300

    Added license

--- snake.c
+++ snake.c
@@ -1,3 +1,21 @@
+/*
+  easy-snake
+  Copyright (C) 2021 Slobodchikov Roman
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -338,7 +356,8 @@ int menu()
 {
     int mode = 0;
 
-    printf( "Enter play mode:\n"
+    printf( "Use WASD to play\n\n"
+            "Enter play mode:\n"
             "1 - Easy\n"
             "2 - Middle\n"
             "3 - Hard\n"
